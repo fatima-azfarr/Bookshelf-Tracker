9320cea26e19fc9e2fc7bfdea861006df39f185e
Fatima Azfar <fatimaazfar310@gmail.com>
2026-08-10 06:14:56 +0500

    Fix Genre/Status enum values

--- backend/schema.py
+++ backend/schema.py
@@ -3,39 +3,42 @@ from enum import Enum
 from pydantic import BaseModel, Field
 
 
-class Genre(str,Enum):
+class Genre(str, Enum):
     fiction = "fiction"
     non_fiction = "nonfiction"
-    sci_fi = "scifi"
+    sci_fi = "sci_fi"
     biography = "biography"
     fantasy = "fantasy"
- 
-class BaseBook(BaseModel):
-    title : str 
-    author : str
-    pages : int | None = None
-    genre : Genre
-    rating : int | None = Field(default = None,ge=1,le=5)
 
 
-class Status(str,Enum):
-    to_read = "toread"
+class Status(str, Enum):
+    to_read = "to_read"
     reading = "reading"
     finished = "finished"
 
+
+class BaseBook(BaseModel):
+    title: str
+    author: str
+    pages: int | None = None
+    genre: Genre
+
+
 class ReadBook(BaseBook):
-    id : int
-    status : Status
-    
+    id: int
+    status: Status
+    rating: int | None = Field(default=None, ge=1, le=5)
+
 
 class AddBook(BaseBook):
     pass
 
+
 class EditBook(BaseBook):
-    status : Status
-    
+    status: Status
+    rating: int | None = Field(default=None, ge=1, le=5)
+
 
 class StatusUpdate(BaseModel):
-    status : Status
-    
-    
+    status: Status
+    rating: int | None = Field(default=None, ge=1, le=5)
